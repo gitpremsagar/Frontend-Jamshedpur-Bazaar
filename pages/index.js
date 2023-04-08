c33006908b193a23dc4694cddb95c07973235c77
Prem Sagar <psagar172@gmail.com>
2023-04-08 11:06:44 +0530

    test commit

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (    
-      <h1 className='text-red-600'>Hello world!</h1>    
+      <h1 className='text-red-600'>Hello world! Again</h1>    
   )
 }
